fc20150145fffbe5c5b3096eb104876736056969
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-26 09:18:24 +0300

    Formatted the document

--- app/main.py
+++ app/main.py
@@ -3,17 +3,17 @@ from fastapi import FastAPI
 
 
 app = FastAPI(
-    title="MySQL CRUD", 
+    title="MySQL CRUD",
     description="A simple app to perform CRUD operations on MySQL"
-    )
+)
 
 
 @app.get(
-    "/", 
-    name="Root", 
-    description="The root endpoint for the app", 
+    "/",
+    name="Root",
+    description="The root endpoint for the app",
     tags=['Root']
-    )
+)
 async def root_endpoint() -> dict[str, str]:
     """The root endpoint for the app"""
     return {"message": "Hello World"}
