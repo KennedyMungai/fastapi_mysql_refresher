d95c304bbdd4d08013e7c3d8aa846e92d9a2efa5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-26 10:04:04 +0300

    Created the UserBase class

--- schemas/user_schemas.py
+++ schemas/user_schemas.py
@@ -1,1 +1,11 @@
 """Created the Schema file for the Users"""
+from pydantic import BaseModel, EmailStr
+
+
+class UserBase(BaseModel):
+    """The base model for the users
+
+    Args:
+        BaseModel (Pydantic): The parent class for the Models
+    """
+    email: EmailStr
