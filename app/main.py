db6d2e62e06afd022f7a1592f8bc5a0a14f432d3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-26 09:18:03 +0300

    Spaced out some definitions

--- app/main.py
+++ app/main.py
@@ -2,10 +2,18 @@
 from fastapi import FastAPI
 
 
-app = FastAPI(title="MySQL CRUD", description="A simple app to perform CRUD operations on MySQL")
+app = FastAPI(
+    title="MySQL CRUD", 
+    description="A simple app to perform CRUD operations on MySQL"
+    )
 
 
-@app.get("/", name="Root", description="The root endpoint for the app", tags=['Root'])
+@app.get(
+    "/", 
+    name="Root", 
+    description="The root endpoint for the app", 
+    tags=['Root']
+    )
 async def root_endpoint() -> dict[str, str]:
     """The root endpoint for the app"""
     return {"message": "Hello World"}
