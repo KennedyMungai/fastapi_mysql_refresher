2271cef97ca9c7d8525d5b213fe2536e97256902
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-26 10:25:34 +0300

    Created the Article Model

--- models/models.py
+++ models/models.py
@@ -20,8 +20,23 @@ class User(Base):
     email = Column(String(255), unique=True, index=True)
     hashed_password = Column(String(255))
     is_active = Column(Boolean, default=True)
-    
-    articles = relationship("Articles", back_populates="author", cascade='all, delete')
+    articles = relationship("Article", back_populates="author", cascade='all, delete')
     
     def __repr__(self):
         return f'{self.email}'
+    
+
+class Article(Base):
+    """The Article Model
+
+    Args:
+        Base (declarative_base): The base for the model
+    """
+    __tablename__ = "articles"
+    id = Column(Integer, primary_key=True, index=True)
+    title=Column(String(50))
+    body=Column(String(255))
+    created_at = Column(DateTime(), default=datetime.now())
+    updated_at = Column(DateTime(), onupdate=datetime.now())
+    author_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
+    author = relationship("User", back_populates="articles")
