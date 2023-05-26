6f5d1d973b67e9409d2a7f5b6e9006defd5fb43c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-26 09:34:56 +0300

    Loaded the environment variables and created the engine using the db url

--- database/db.py
+++ database/db.py
@@ -5,3 +5,11 @@ from dotenv import find_dotenv, load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+load_dotenv(find_dotenv())
+
+MYSQL_USER = os.environ.get('MYSQL_USER')
+MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
+MYSQL_DB = os.environ.get('MYSQL_DB')
+
+engine = create_engine(f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@localhost:3306/{MYSQL_DB}')
