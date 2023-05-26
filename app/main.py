2c67185783c46ae154d0984d0702c9360f36501c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-26 09:17:28 +0300

    Some simple setup code for the app

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
 """The main script for the app"""
+from fastapi import FastAPI
+
+
+app = FastAPI(title="MySQL CRUD", description="A simple app to perform CRUD operations on MySQL")
+
+
+@app.get("/", name="Root", description="The root endpoint for the app", tags=['Root'])
+async def root_endpoint() -> dict[str, str]:
+    """The root endpoint for the app"""
+    return {"message": "Hello World"}
