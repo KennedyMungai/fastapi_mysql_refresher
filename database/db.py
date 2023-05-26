f08ac3663f06bfd97060ca46b902d5889b82b035
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-26 09:36:03 +0300

    Created SessionLocal and Base

--- database/db.py
+++ database/db.py
@@ -13,3 +13,7 @@ MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
 MYSQL_DB = os.environ.get('MYSQL_DB')
 
 engine = create_engine(f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@localhost:3306/{MYSQL_DB}')
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
