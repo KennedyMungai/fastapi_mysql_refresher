2542857c281fbb88783d642342b8cf559ff5a0b3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-26 10:12:19 +0300

    Added imports

--- models/models.py
+++ models/models.py
@@ -1,1 +1,7 @@
 """The models logic"""
+from datetime import datetime
+
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from database.db import Base
