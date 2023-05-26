bbd860d1a0661e317fd7c4dc80b6bc93098fae67
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-26 10:15:55 +0300

    Created the base user model

--- models/models.py
+++ models/models.py
@@ -5,3 +5,12 @@ from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from database.db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(255), unique=True, index=True)
+    hashed_password = Column(String(255))
+    is_active = Column(Boolean, default=True)
