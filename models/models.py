7d3b314b42f30530716c4c82ab622aa5ad7748d1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-26 10:19:06 +0300

    Added a docstring for the User model

--- models/models.py
+++ models/models.py
@@ -8,9 +8,20 @@ from database.db import Base
 
 
 class User(Base):
+    """The User model for the db
+
+    Args:
+        Base (declarative_base): The base for models
+   
+    """
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(255), unique=True, index=True)
     hashed_password = Column(String(255))
     is_active = Column(Boolean, default=True)
+    
+    articles = relationship("Articles", back_populates="author", cascade='all, delete')
+    
+    def __repr__(self):
+        return f'{self.email}'
