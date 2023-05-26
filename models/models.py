ce33f4236f215100b614f25e8a4b2b9a97c95a73
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-26 10:28:23 +0300

    Made some spacing updates

--- models/models.py
+++ models/models.py
@@ -12,19 +12,20 @@ class User(Base):
 
     Args:
         Base (declarative_base): The base for models
-   
+
     """
     __tablename__ = "users"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(255), unique=True, index=True)
     hashed_password = Column(String(255))
     is_active = Column(Boolean, default=True)
-    articles = relationship("Article", back_populates="author", cascade='all, delete')
-    
+    articles = relationship(
+        "Article", back_populates="author", cascade='all, delete')
+
     def __repr__(self):
         return f'{self.email}'
-    
+
 
 class Article(Base):
     """The Article Model
@@ -34,8 +35,8 @@ class Article(Base):
     """
     __tablename__ = "articles"
     id = Column(Integer, primary_key=True, index=True)
-    title=Column(String(50))
-    body=Column(String(255))
+    title = Column(String(50))
+    body = Column(String(255))
     created_at = Column(DateTime(), default=datetime.now())
     updated_at = Column(DateTime(), onupdate=datetime.now())
     author_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
